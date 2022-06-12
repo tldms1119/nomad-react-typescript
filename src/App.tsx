35c11f46382e4ab0316f95d7bdcff351f42df325
sieun <tldms1119@naver.com>
2022-06-12 22:19:56 +0900

    nomad-react with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {currentTarget: {value}} = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" placeholder="username" value={value} onChange={onChange} onSubmit={onSubmit}/>
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
